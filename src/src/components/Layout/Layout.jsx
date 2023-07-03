e2e28a9987b062673fa39df2ff0919718a7a0699
Your Name <christofaokoro@gmail.com>
2023-07-03 08:57:50 +0100

    blog setup and edits

---
+++ src/src/components/Layout/Layout.jsx
@@ -0,0 +1,27 @@
+// // eslint-disable-next-line no-unused-vars
+// import React from "react"
+
+// import Navbar from "../Navbar/Navbar"
+// import Footer from "../Footer/Footer"
+
+// function Layout({children}) {
+//   return (
+//     <div>
+//         <Navbar/>
+//         {children}
+//         <Footer/>
+//     </div>
+//   )
+// }
+
+// // These are the types of the props that are passed to the component
+
+// Layout.propTypes = {};
+
+// // These are their default values
+
+// Layout.defaultProps = {};
+
+// export default Layout
+
+
